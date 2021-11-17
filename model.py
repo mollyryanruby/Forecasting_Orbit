e9a10832f649e8c4421413e4455e6b997d44fe74
Molly Liebeskind <mollyliebeskind@Mollys-MacBook-Pro.local>
2021-11-17 16:35:33 -0600

    added  compenent plotting functionality

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import pathlib
 import time
+import os
 
 start_time = time.time()
 
@@ -26,13 +27,15 @@ for package in modelpackages:
 
 from orbit.models.dlt import ETSFull, DLTMAP, DLTFull
 from orbit.models.lgt import LGTMAP, LGTFull, LGTAggregated
-# from orbit.diagnostics.plot import plot_predicted_data
+from orbit.diagnostics.plot import plot_predicted_data, plot_predicted_components
 from pmdarima.arima import auto_arima
 
 
 ## Instantiate Variables
-dataPath = str(pathlib.Path().resolve()) + "/data/"
+dirPath = str(pathlib.Path().resolve()) 
+dataPath = dirPath + "/data/"
 rawDataPath = dataPath + 'train.csv.zip'
+imgPath = dirPath + "/img/"
 date_col = 'date'
 response_col = 'sales'
 
@@ -53,10 +56,17 @@ def mape(actual, pred):
     actual, pred = np.array(actual), np.array(pred)
     return np.mean(np.abs((actual - pred) / actual)) * 100
 
-def runTheModel(df_train, df_test, model, modName, date_col, response_col):
+def runTheModel(df_train, df_test, model, modName, date_col, response_col, decompose=False, imgPath=imgPath):
     if not 'ARIMA' in modName:
         model.fit(df_train)
-        pred = model.predict(df_test[[date_col]]).prediction.unique()
+        if not decompose:
+            pred = model.predict(df_test[[date_col]]).prediction.unique()
+        else:
+            pred = model.predict(df_train, decompose=True)
+            vis = plot_predicted_components(pred, date_col, is_visible=False,
+                                            plot_components=['prediction', 'trend', 'seasonality'],
+                                            path=imgPath)
+            return vis, pred
     else:
         pred = model.predict(df_test.shape[0])
 
@@ -66,89 +76,91 @@ def runTheModel(df_train, df_test, model, modName, date_col, response_col):
     # print(prediction_df.head())
     return prediction_df
 
-## Load and Process Data
-df = pd.read_csv(rawDataPath)
-df = preprocessData(df, date_col, response_col)
-train, test = trainTestestSplit(df)
-
-# Instantiate model dictionary
-models = {'ETSFull': ETSFull(
+def getModelDict(train):
+    modelDict = {'ETSFull': ETSFull(
                     response_col=response_col,
                     date_col=date_col,
                     seasonality=12,
                     seed=8888
                     ),
-            'DLTMAP_Linear': DLTMAP(
-                    response_col=response_col,
-                    date_col=date_col,
-                    seasonality=12,
-                    seed=8888
-                    ),
-            'DLTMAP_LogLin': DLTMAP(
-                            response_col=response_col,
-                            date_col=date_col,
-                            seasonality=12,
-                            seed=8888,
-                            global_trend_option='loglinear'
-                            ),
-            'DLTMAP_Logistic': DLTMAP(
-                                response_col=response_col,
-                                date_col=date_col,
-                                seasonality=12,
-                                seed=8888,
-                                global_trend_option='logistic'
-                                ),
-            'LGTMAP': LGTMAP(
+                'DLTMAP_Linear': DLTMAP(
                         response_col=response_col,
                         date_col=date_col,
                         seasonality=12,
                         seed=8888
-                        ), # Commented out because the results were too poor
-            'LGTFull': LGTFull(
-                        response_col=response_col,
-                        date_col=date_col,
-                        seasonality=12,
-                        seed=8888,
-                    ),
-            'LGTAggregation': LGTAggregated(
+                        ),
+                'DLTMAP_LogLin': DLTMAP(
                                 response_col=response_col,
                                 date_col=date_col,
                                 seasonality=12,
                                 seed=8888,
-                            ),
-            'DLTFull': DLTFull(
-                        response_col=response_col,
-                        date_col=date_col,
-                        seasonality=12,
-                        seed=8888,
-                        num_warmup=5000,
-                    ),
-           'ARIMA': auto_arima(train[[response_col]],
-                                m=12,
-                                seasonal=True
-                                ) }
-        
+                                global_trend_option='loglinear'
+                                ),
+                'DLTMAP_Logistic': DLTMAP(
+                                    response_col=response_col,
+                                    date_col=date_col,
+                                    seasonality=12,
+                                    seed=8888,
+                                    global_trend_option='logistic'
+                                    ),
+                'LGTMAP': LGTMAP(
+                            response_col=response_col,
+                            date_col=date_col,
+                            seasonality=12,
+                            seed=8888
+                            ), # Commented out because the results were too poor
+                'LGTFull': LGTFull(
+                            response_col=response_col,
+                            date_col=date_col,
+                            seasonality=12,
+                            seed=8888,
+                        ),
+                'LGTAggregation': LGTAggregated(
+                                    response_col=response_col,
+                                    date_col=date_col,
+                                    seasonality=12,
+                                    seed=8888,
+                                ),
+                'DLTFull': DLTFull(
+                            response_col=response_col,
+                            date_col=date_col,
+                            seasonality=12,
+                            seed=8888,
+                            num_warmup=5000,
+                        ),
+            'ARIMA': auto_arima(train[[response_col]],
+                                    m=12,
+                                    seasonal=True
+                                    ) }
+    return modelDict
 
-# Run the models
-predictions = []
+## Load and Process Data
+if __name__ == '__main__':
+    df = pd.read_csv(rawDataPath)
+    df = preprocessData(df, date_col, response_col)
+    train, test = trainTestestSplit(df)
 
-for mod in models:
-    print('running', mod)
-    predictions.append(runTheModel(train, test, models[mod], mod, date_col, response_col))
+    models = getModelDict(train, test)
 
-# Create dataframe of all results
-full_df = pd.concat(predictions)
-full_df['Rank'] = full_df.MAPE.rank(method='dense')
+    # Run the models
+    predictions = []
 
-# Create dataframe of model rankings
-ranking_df = full_df[['Model', 'Rank', 'MAPE']].drop_duplicates().sort_values(by='Rank')
+    for mod in models:
+        print('running', mod)
+        predictions.append(runTheModel(train, test, models[mod], mod, date_col, response_col))
 
-# Write data
-full_df.to_csv(dataPath + 'model_output.csv', index=False)
-ranking_df.to_csv(dataPath + 'ranking.csv', index=False)
-df.to_csv(dataPath + 'procssed_data.csv', index=False)
-# train.to_csv(dirpath + "train.csv", index=False)
-# test.to_csv(dirpath + 'test.csv', index=False)
+    # Create dataframe of all results
+    full_df = pd.concat(predictions)
+    full_df['Rank'] = full_df.MAPE.rank(method='dense')
 
-print("Application took:", time.time() - start_time, "to run.")
+    # Create dataframe of model rankings
+    ranking_df = full_df[['Model', 'Rank', 'MAPE']].drop_duplicates().sort_values(by='Rank')
 
+    # Write data
+    full_df.to_csv(dataPath + 'model_output.csv', index=False)
+    ranking_df.to_csv(dataPath + 'ranking.csv', index=False)
+    df.to_csv(dataPath + 'procssed_data.csv', index=False)
+    # train.to_csv(dirpath + "train.csv", index=False)
+    # test.to_csv(dirpath + 'test.csv', index=False)
+
+    print("Application took:", time.time() - start_time, "to run.")
