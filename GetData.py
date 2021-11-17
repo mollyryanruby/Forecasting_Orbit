d1b186a42cd3283923a4a1740d38ccd1a608ac4c
Molly Liebeskind <mollyliebeskind@Mollys-MacBook-Pro.local>
2021-11-17 15:02:33 -0600

    revised dataset to Kaggle from housing data on quanld

--- GetData.py
+++ GetData.py
@@ -15,43 +15,54 @@ def checkForPackages(package):
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 try:
-    import quandl as ql
+    import kaggle
 except:
-    checkForPackages('quandl')
+    checkForPackages('kaggle')
 
-def getDataPerZip(zip):
-    countyData = ql.get_table("ZILLOW/REGIONS", region_type='zip', paginate=True)
-    regions = countyData[countyData.region.str.contains(zip)].region_id.unique()
+# kaggle competitions download -c demand-forecasting-kernels-only -f test.csv.7z
+from kaggle.api.kaggle_api_extended import KaggleApi
 
-    if len(regions) == 1:
-        priceData = ql.get_table("ZILLOW/DATA", 
-                                indicator_id='ZALL', 
-                                region_id=regions[0], 
-                                paginate=True)
-    else:
-        print("Multiple regions selected")  
-        priceData = ql.get_table("ZILLOW/DATA", 
-                                indicator_id='ZSFH',
-                                paginate=True)
-        priceData = priceData.filter(priceData.region_id.str.isin(regions))
+dirPath = str(pathlib.Path().resolve())
+dataPath =  dirPath + "/data/"
+api = KaggleApi()
+api.authenticate()
+
+api.competition_download_file('demand-forecasting-kernels-only','train.csv', path=dataPath)
+
+
+# def getDataPerZip(zip):
+#     countyData = ql.get_table("ZILLOW/REGIONS", region_type='zip', paginate=True)
+#     regions = countyData[countyData.region.str.contains(zip)].region_id.unique()
+
+#     if len(regions) == 1:
+#         priceData = ql.get_table("ZILLOW/DATA", 
+#                                 indicator_id='ZCON', 
+#                                 region_id=regions[0], 
+#                                 paginate=True)
+#     else:
+#         print("Multiple regions selected")  
+#         priceData = ql.get_table("ZILLOW/DATA", 
+#                                 indicator_id='ZCON',
+#                                 paginate=True)
+#         priceData = priceData.filter(priceData.region_id.str.isin(regions))
     
-    return priceData
+#     return priceData
 
-def filterDates(df):
-    df.date = pd.to_datetime(df.date, format='%Y-%m-%d')
-    df2 = df[(df.date >= '2010-01-01') & (df.date < '2021-11-01')]
-    return df2
+# def filterDates(df):
+#     df.date = pd.to_datetime(df.date, format='%Y-%m-%d')
+#     df2 = df[(df.date >= '2016-01-01') & (df.date < '2020-01-01')]
+#     return df2
 
-ql.ApiConfig.api_key = "3shXhW8vy7tPPKoazXwb"
+# ql.ApiConfig.api_key = "3shXhW8vy7tPPKoazXwb"
 
-df = getDataPerZip('10023')
-df = filterDates(df)
+# df = getDataPerZip('10023')
+# df = filterDates(df)
 
-dirpath = str(pathlib.Path().resolve()) + "/data/"
+# dirpath = str(pathlib.Path().resolve()) + "/data/"
 
-if not os.path.exists(dirpath):
-    os.mkdir(dirpath)
+# if not os.path.exists(dirpath):
+#     os.mkdir(dirpath)
 
-path = dirpath + 'raw.csv'
+# path = dirpath + 'raw.csv'
 
-df.to_csv(path)
+# df.to_csv(path)
